7eb474bd7cdfc2e00f3f39c9406fa342d9b9ac14
Stephen <datastructuretong@foxmail.com>
2021-07-25 18:07:19 +0800

    first commit

---
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aiguigu.git;
+
+/**
+ * @author Stephen Tong
+ * @create 2021-07-25 18:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
