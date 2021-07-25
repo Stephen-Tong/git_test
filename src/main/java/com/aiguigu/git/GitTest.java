c4bdf52a87afa150ac37ffde88c7fa8699b1bd17
Stephen <datastructuretong@foxmail.com>
2021-07-25 18:29:12 +0800

    hot-fix commit 1

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello GitHub");
         System.out.println("hello PornHub");
+        System.out.println("hello Wd/Hub");
     }
 }
